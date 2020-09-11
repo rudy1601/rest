4e9982fce1ac15ea3a752aeb4ce12512283752f0
andriTinas <andrianustinas.siskom@gmail.com>
2020-09-11 17:13:48 +0700

    tambah postingan

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,13 +11,35 @@ class PostController extends Controller
     public function list()
     {
         $posts = Post::get()->map(function ($item) {
-            $data['id']     = $item->id;
-            $data['title']  = $item->title;
-            $data['body']   = $item->body;
-            $data['author'] = $item->user->name;
-
-            return $data;
+            return $this->singleRest($item);
         });
+        
         return response()->json($posts, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $post = Post::create([
+            'title'     => $request->title,
+            'body'      => $request->body,
+            'user_id'    => $this->author()->id
+        ]);
+        
+        return response()->json($this->singleRest($post),200);
+    }
+
+    private function author()
+    {
+        return User::first();
+    }
+
+    private function singleRest(Post $post)
+    {
+        return [
+            'id'        => $post->id,
+            'title'     => $post->title,
+            'body'      => $post->body,
+            'author'    => $post->user->name
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'posts'], function () {
     Route::get('/', 'PostController@list')->name('list');
+    Route::post('/', 'PostController@store')->name('store');
 });
